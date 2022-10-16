70af1abff1e981ce2ad3d83bdc971a236670ae61
MitchBrenner <mkbrenner3@wisc.edu>
2022-10-16 00:39:04 -0500

    Updated App
    Added title, search bar, search results

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
+import React, { setState, useState } from 'react';
 import './App.css';
 
+// var movieList = [];
+// movieList.push("harry potter");
+
+
 function App() {
+  const[movies, setMovie] = useState(["Harry Potter", "Lord of the Rings", "Smile", "Back to the Future", "King Kong"])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div id = "movieSearchDiv">
+        <div className='searchMovies'>
+          <SearchMovies />
+        </div>
+        <div className='movies'>
+          <MovieRow movies = {movies} />
+        </div>
+      </div>
     </div>
   );
+}
+
+function Header() {
+  return(
+    <header>
+      <h1 id = "title">NextFlix</h1>
+    </header>
+  )
+}
+
+function SearchMovies(){
+  return(
+    <div className = "movieDiv">
+      <h3 id = "searchTitle">Search For Movies</h3>
+      <input type = "search" id = "search"/>
+    </div>
+  )
+}
+
+function MovieRow({ movies }){
+  return(
+    movies.map(movie => {
+      return(
+        <div className = "movieRow">
+          <p className='movie'>{movie}</p>
+          <button id = "addMovie">Add</button>
+        </div>
+      )
+    })
+  )
 }
 
 export default App;
